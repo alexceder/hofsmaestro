cc747ebf7720b58ad78be54ce09a4faed1729200
Philburridge <burridge@live.se>
2014-05-24 13:18:44 +0200

    Added getlocation

--- app/views/start.blade.php
+++ app/views/start.blade.php
@@ -1,4 +1,9 @@
 @extends('master')
 @section('main-content')
-	
+	<h1>Find location</h1>
+    <p id="demo">location.</p>
+
+    <button onclick="getLocation()">Get pos</button>
+
+    <script src="js/location.js"></script>
 @stop
